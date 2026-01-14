4405ccb256aaf58e9c64d27ed16a0fcbb801ba68
Finnbur <burghaus.finn@gmail.com>
2026-01-14 16:42:23 +0100

    Migrate Kyreth to PHP and update asset paths
    Replaced Kyreth's index.html with index.php, added core and index CSS files, and moved kyreth.png to the img directory. Updated login.html to reference the new PHP entry point and image path.

---
+++ Kyreth/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Kyreth - Home</title>
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    <body>
+        <img src="img/kyreth.png">
+        <p>Kyreth</p>
+    </body>
+</html>
