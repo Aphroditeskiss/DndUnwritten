a5c4c1d789d1743b70420f0f276a05dfe871367f
Sam <samdelhaye06@gmail.com>
2026-01-16 16:24:33 +0100

    Redirect Khaos/index.php and move old content to index123.php
    Khaos/index.php now redirects to an external D&D Beyond character page. The previous HTML content has been moved to a new file, Khaos/index123.php, preserving the original page structure and content.

---
+++ Khaos/index123.php
@@ -0,0 +1,26 @@
+<?php
+
+require('inc/functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+head("Khaos!")
+    ?>
+
+<body>
+    <?php
+    khaosHeader();
+    ?>
+    <h1>KHAOS</h1>
+    <img src="images/khaos.jpg" alt="khaos">
+    <p>"My name is Khaos. <br>
+        I was meant to be here, even if I took a strange path to get here. <br>
+        Some connections go deeper than memory... and some of you might understand, in time."
+    </p>
+        <a href="secret">Backstory and personality traits</a>
+</body>
+
+</html>
