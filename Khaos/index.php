513dd0a1b571f433b6c9caa957aaab02c923881a
Sam <samdelhaye06@gmail.com>
2026-01-17 01:40:58 +0100

    Refactor Khaos app structure and update index page
    Moved all files from Khaos/ to not_Khaos/, including images and supporting assets. Updated Khaos/index.php with new HTML structure, added style.css for basic layout and styling, and improved header/footer presentation. The original PHP-based index page is now in not_Khaos/index.php, while the new Khaos/index.php uses static HTML and CSS.

--- Khaos/index.php
+++ Khaos/index.php
@@ -1,26 +1,32 @@
 <?php
 
-require('inc/functions.php');
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php
-head("Khaos!");
-?>
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Khaos!</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/khaos.gif" type="image/x-icon">
+</head>
 <body>
-    <?php
-    khaosHeader();
-    ?>
-    <h1>KHAOS</h1>
-    <img src="images/khaos.jpg" alt="khaos">
-    <p>"My name is Khaos. <br>
-        I was meant to be here, even if I took a strange path to get here. <br>
-        Some connections go deeper than memory... and some of you might understand, in time."
-    </p>
-        <a href="secret">Backstory and personality traits</a>
-</body>
+    <header>
+        <img src="images/khaos.jpg">
+        Khaos!
+    </header>
 
+    <h1>Khaos!</h1>
+    <h2>Twink king</h2>
+    <ul>
+        <li>twink</li>
+        <li>ultimate Rizzler</li>
+        <li>ver good looking</li>
+    </ul>
+    <img src="images/khaos.jpg">
+    <img src="images/khaos.gif">
+
+    <footer>&copy; Khaos 2026</footer>
+</body>
 </html>
